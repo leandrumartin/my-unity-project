4ca7d28bc732058c1f5458de3d15575f66113407
Leandru Martin <leandru.martin@slu.edu>
2024-07-04 14:07:48 -0500

    Completed Chapter 15

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    public float amount;
+
+    void Update()
+    {
+        if (amount < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
